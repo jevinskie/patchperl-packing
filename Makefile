590759dab5c945c9fff6fc4093b910824e75c4b3
Jevin Sweval <jevinsweval@gmail.com>
2024-08-30 23:33:43 -0400

    build h4x

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 all:
-	fatpack trace `which patchperl`
+	fatpack trace "$(PERLBREW_ROOT)/perls/$(PERLBREW_PERL)/bin/patchperl"
 	fatpack packlists-for `cat fatpacker.trace` >packlists
 	fatpack tree `cat packlists`
-	(echo "#!/usr/bin/env perl"; fatpack file; cat `which patchperl`) > patchperl
+	(echo "#!/usr/bin/env perl"; fatpack file; cat "$(PERLBREW_ROOT)/perls/$(PERLBREW_PERL)/bin/patchperl")  > patchperl.manyshebangs
+	awk '!(/^#!\// && ++c > 1)' patchperl.manyshebangs > patchperl
+	rm -f patchperl.manyshebangs
 	chmod +x patchperl
 
 
--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 perlenv=perl-5.8.8@patchperl-packing
 
 cd $(dirname $0)
 
 eval "$(perlbrew init-in-bash)"
-perlbrew use ${perlenv}
+# perlbrew use ${perlenv}
+true
 
 if [[ $? -eq 0 ]]; then
     echo "-- Using to $perlenv"
@@ -22,21 +23,23 @@ cpanm Devel::PatchPerl App::FatPacker || exit 1
 hash -r
 hash -r
 
-if [[ "$PERLBREW_ROOT/bin/patchperl" == $(which patchperl) ]]; then
+if [[ "$PERLBREW_ROOT/bin/patchperl" == "${PERLBREW_ROOT}/perls/${PERLBREW_PERL}/bin/patchperl" ]]; then
     echo "The patch of patchperl does not look right, abort"
     exit 1
 fi
 
 
-fatpack trace `which patchperl`
+fatpack trace "${PERLBREW_ROOT}/perls/${PERLBREW_PERL}/bin/patchperl"
 fatpack packlists-for `cat fatpacker.trace` >packlists
 fatpack tree `cat packlists`
-(echo "#!/usr/bin/env perl"; fatpack file; cat `which patchperl`) > patchperl
+(echo "#!/usr/bin/env perl"; fatpack file; cat "${PERLBREW_ROOT}/perls/${PERLBREW_PERL}/bin/patchperl") > patchperl.manyshebangs
+awk '!(/^#!\// && ++c > 1)' patchperl.manyshebangs > patchperl
+rm -f patchperl.manyshebangs
 chmod +x patchperl
 
 versions=$(perl -MApp::FatPacker -MDevel::PatchPerl -e 'print "patchperl: " . Devel::PatchPerl->VERSION . ", fatpacker: " . App::FatPacker->VERSION . "\n"')
 
-git add patchperl
+# git add patchperl
 
 git_changed=$(git status --porcelain patchperl | grep patchperl)
 
@@ -44,9 +47,10 @@ if [[ "$git_changed" == "" ]]; then
     echo "Nothing changed. Skip committing."
     exit 0
 else
-    git commit -m "rebuild with $versions"
-    git push
-    git clean -d -f
-    git pull
+    true
+    # git commit -m "rebuild with $versions"
+    # git push
+    # git clean -d -f
+    # git pull
 fi
 
